ad0d085fa37aaaeb7b1e41ca5a768ef4818f93fa
negi0109 <negi0109.seg@gmail.com>
2020-09-10 09:44:23 +0900

    階乗2

--- app/Main.hs
+++ app/Main.hs
@@ -6,5 +6,8 @@ helloWorld = putStrLn "hello World!"
 
 fac1 n = if n == 0 then 1 else n * fac1(n - 1)
 
+fac2 0 = 1
+fac2 n = n * fac2(n - 1)
+
 main :: IO ()
-main = print(fac1 3)
+main = print(fac2 3)
