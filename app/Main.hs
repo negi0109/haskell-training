a93cc504b026fcd76bb3bb9228ec323467eae512
negi0109 <negi0109.seg@gmail.com>
2020-09-17 00:12:18 +0900

    array

--- app/Main.hs
+++ app/Main.hs
@@ -9,5 +9,12 @@ fac1 n = if n == 0 then 1 else n * fac1(n - 1)
 fac2 0 = 1
 fac2 n = n * fac2(n - 1)
 
+-- [1,2,3] ++ [4] => [1,2,3,4]
+concat' = (++)
+-- 10:[1,2,3] => [10,1,2,3,4]
+add' = (:)
+-- [1,2,3]!!2 => 3
+get' = (!!)
+
 main :: IO ()
 main = print(fac2 3)
